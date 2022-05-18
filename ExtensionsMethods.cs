abb09b09d6b273674b4734242a30a3de32504d71
Serhii Radchuk <serhii@syxsense.com>
2022-05-18 14:09:14 +0000

    Full theory rep

---
+++ ExtensionsMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theory_Project
+{
+    public static class ExtensionsMethods
+    {
+        public static bool IsDuplicatePresent(this List<string> list)
+        {
+            foreach (string item in list)
+            {
+                int index1 = list.IndexOf(item);
+                int index2 = list.LastIndexOf(item);
+                if (!index1.Equals(index2))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
