4b6adab62117f21b8159408d2e5744179fbc8c5c
Serhii Rachuk (acer) <radchuk-s@ukr.net>
2023-07-19 12:29:21 +0200

    tcp

---
+++ TCP_Connection/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Connection
+{
+    internal class Client
+    {
+        public static void Start(string message, string serverIp = "127.0.0.1", int port = 23)
+        {
+            try
+            {
+                // Create a TCP client object and connect to the server
+                TcpClient client = new TcpClient(serverIp, port);
+                Console.WriteLine("Connected to server: {0}:{1}", serverIp, port);
+
+                // Get the network stream for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                // Send data to the server
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Data sent to server: {0}", message);
+
+                // Read the response from the server
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Response received from server: {0}", response);
+
+                // Close the connection
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: {0}", ex.Message);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP_Connection/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TCP_Connection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string internalIpAddress = GetInternalIpAddress(); // 192.168.243.8
+            //string externalIpAddress = GetExternalIpAddress(); // 186.127.227.102
+
+            //Client.Start("Message from client to server", "192.168.1.132", 5205);
+            Server.Start("Message from server to client", "192.168.243.8");
+        }
+
+        static string GetInternalIpAddress()
+        {
+            // Get the local IP address
+            string ipAddress = string.Empty;
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip.ToString();
+                    break;
+                }
+            }
+            return ipAddress;
+        }
+
+        static string GetExternalIpAddress()
+        {
+            try
+            {
+                // Use a web service to get the external IP address
+                WebClient webClient = new WebClient();
+                string ipAddress = webClient.DownloadString("https://api.ipify.org");
+                return ipAddress;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while getting the external IP address: {0}", ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+}
